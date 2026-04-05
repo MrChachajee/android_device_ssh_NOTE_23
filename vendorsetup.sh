4e95896fe4ffd243e463ff33aa8b33313c20821d
MrChachajee <69984797+MrChachajee@users.noreply.github.com>
2026-04-05 17:38:07 +0500

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,10 @@
 #
 # Copyright (C) 2026 The Android Open Source Project
-# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2026 SebaUbuntu's omni device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo twrp_NOTE_23-user
-add_lunch_combo twrp_NOTE_23-userdebug
-add_lunch_combo twrp_NOTE_23-eng
+add_lunch_combo omni_NOTE_23-user
+add_lunch_combo omni_NOTE_23-userdebug
+add_lunch_combo omni_NOTE_23-eng
