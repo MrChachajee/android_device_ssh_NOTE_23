f20f5e4eb1eb20fe7ff7c1c75af33057893d04e2
MrChachajee <69984797+MrChachajee@users.noreply.github.com>
2026-04-05 17:37:02 +0500

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_NOTE_23-user
-add_lunch_combo omni_NOTE_23-userdebug
-add_lunch_combo omni_NOTE_23-eng
+add_lunch_combo twrp_NOTE_23-user
+add_lunch_combo twrp_NOTE_23-userdebug
+add_lunch_combo twrp_NOTE_23-eng
