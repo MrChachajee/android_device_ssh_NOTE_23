e37950531965d4f5f179f68a5a6d152e8acca28f
MrChachajee <69984797+MrChachajee@users.noreply.github.com>
2026-04-05 17:41:35 +0500

    Update and rename omni_NOTE_23.mk to twrp_NOTE_23.mk

--- twrp_NOTE_23.mk
+++ twrp_NOTE_23.mk
@@ -9,14 +9,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common Twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from NOTE_23 device
 $(call inherit-product, device/ssh/NOTE_23/device.mk)
 
 PRODUCT_DEVICE := NOTE_23
-PRODUCT_NAME := omni_NOTE_23
+PRODUCT_NAME := twrp_NOTE_23
 PRODUCT_BRAND := VGO_TEL
 PRODUCT_MODEL := NOTE 23
 PRODUCT_MANUFACTURER := ssh
