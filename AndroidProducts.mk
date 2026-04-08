034646e265e4b08697dcbcff6a991df175c62fd0
MrChachajee <69984797+MrChachajee@users.noreply.github.com>
2026-04-09 00:34:14 +0500

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2026 The Android Open Source Project
+# Copyright (C) 2026 The android Open Source Project
 # Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
@@ -9,6 +9,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/omni_NOTE_23.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_NOTE_23-android-user \
-    omni_NOTE_23-android-userdebug \
-    omni_NOTE_23-android-eng
+    omni_NOTE_23-ap2a-user \
+    omni_NOTE_23-ap2a-userdebug \
+    omni_NOTE_23-ap2a-eng
